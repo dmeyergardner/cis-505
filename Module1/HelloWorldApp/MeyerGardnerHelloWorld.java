089b179d3b8196053f1bcf1b37570081f305ffaf
dmeyergardner <128332832+dmeyergardner@users.noreply.github.com>
2023-03-19 22:22:37 -0400

    Create MeyerGardnerHelloWorld.java

---
+++ Module1/HelloWorldApp/MeyerGardnerHelloWorld.java
@@ -0,0 +1,20 @@
+package HelloWorldApp;
+
+/**
+ *
+ * @author deborahmeyer-gardner
+ */
+public class MeyerGardnerHelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // Create a program that output’s the String value “Hello World from YourFirstName YourLastName.”
+        // Use String variables for the first and last name values.
+        String yourFirstNameString = "Deborah";
+        String yourLastNameString = "Meyer-Gardner";
+        //Use printf to write the results to the System.out window.
+        System.out.printf("Hello World from " + yourFirstNameString + " " + yourLastNameString); 
+    }
+}
