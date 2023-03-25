6632cca93865e65c33c17fcb639d64eb408a4068
dmeyergardner <128332832+dmeyergardner@users.noreply.github.com>
2023-03-24 20:30:56 -0400

    practicing creating a jar file, these are HelloWorlds

---
+++ Module_2/JarPracticeFiles/MeyerGardnerJarTwo.java
@@ -0,0 +1,13 @@
+/*
+    Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: 
+        Comprehensive Version (12th ed.). Pearson Education, Inc.
+    Modified by R. Krasso 2021
+    Additional modifications by D. Meyer-Gardner 2023
+*/ 
+
+public class MeyerGardnerJarTwo {
+    // Your First Program
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
