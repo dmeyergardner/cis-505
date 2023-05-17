bce7923068761428eb6f6b84cf2c8feb50ac3d44
dmeyergardner <128332832+dmeyergardner@users.noreply.github.com>
2023-05-16 21:22:04 -0400

    FutureValueApp v0.2

--- FutureValueApp/MeyerGardenerFutureValueApp.java
+++ FutureValueApp/MeyerGardenerFutureValueApp.java
@@ -5,13 +5,8 @@
     Modified by R. Krasso 2021
     Additional modifications by D. Meyer-Gardner 2023
 
-    Download and install the JavaFX SDK.
-2. Add a new folder to your cis-505 repository and name it FutureValueApp.
-3. Add a new file to the FutureValueApp folder and name it YourLastNameFutureValueApp.java.
 4. Extend the JavaFX Application object and setup the @Override start method with a single argument for the Stage object. Exhibit B @Override start(Stage primaryStage)
 5. Create private variables for two TextField’s, one TextArea, five Labels, one ComboBox of type Integer, and two Button’s.
-
-Additional Java Programming Requirements
 a. Set the text for the labels to match the labels in Exhibit A. Exhibit C Labels with a default text value
 b. Set the text for the buttons to match the buttons in Exhibit A. Exhibit D Buttons with a default text value   
 c. In the start() method, add a new GridPane object and set its alignment to Pos.CENTER, set its padding to 11.5, 12.5, 13.5, and 14.5. Next, set the panes Hgap to 5.5 and its Vgap to 5.5.
@@ -21,38 +16,58 @@ f. Set the primary stages title to “YourLastName Future Value App.”
 */
 
 
-
+//Extend the JavaFX Application object 
 public class MeyerGardenerFutureValueApp extends Application {
 
   @Override // Override the start method in the Application class
   public void start(Stage primaryStage) {
 
+    //In the start() method, add a new GridPane object and set its alignment to Pos.CENTER, set its padding to 11.5, 12.5, 13.5, and 14.5. Next, set the panes Hgap to 5.5 and its Vgap to 5.5
+
+
+    // Add the controls to the GridPane. For the lblInterestRateFormat label, set the text fill to red and HPos to the right.
+    lblInterestRateFormat.setTextFill(Color.RED); //Set the font color of the interest rate instructions to red
+    pane.add(lblInterestRateFormat, 1, 2); //Add the label to the grid pane
+    GridPane.setHalignment(lblInterestRateFormat, HPos.RIGHT); //Postition the label to the right of the pane
+
+    //Add the clear and calculate buttons to an HBox with a spacing of 10 and a padding of 15, 0, 15, and 30.
+    HBox actionBtnContainer = new HBox (); //Create new Hbox container
+    actionBtnContainer.setPadding(new Insets(15, 0, 15, 30)) //set container padding
+    actionBtnContainer.setSpacing(10); //set container spacing
+    actionBtnContainer.getChildren().add(btnClear); //add btnClear to container
+    actionBtnContainer.getChildren().add(btnCalculate); //add btnCalculate to container
+    pane.add(actionBtnContainer, 1, 4); //add the container to the grid panel
+
     // set title for the stage
     s.setTitle("Future Value App");
  
-    // create a textfield
+    // create two textfield
     private TextField txtMonthlyPayment = new TextField();
     private TextField b = new TextField();
 
     // create a stack pane
     StackPane r = new StackPane();
  
-    // add textfield
+    // add one textfield
     r.getChildren().add(b);
 
-    // create a label
-    private Label lblMonthlyPayment = new Label("Monthly Payment");
-    private Label lblInterestRate = new Label("Interest Rate");
-    private Label lblYears = new Label("Years");
+    // Create a combo box of type Integer
+    //ComboBox combo_box = new ComboBox(FXCollections.observableArrayList(week_days));
+
+    // create five label
+    private Label lblMonthlyPayment = new Label("Monthly Payment:");
+    private Label lblInterestRate = new Label("Interest Rate:");
+    private Label lblYears = new Label("Years:");
     private Label lblClear = new Label("Clear");
     private Label lblCalculate = new Label("Calculate");
 
-    // Weekdays
-    String week_days[] =
-    { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
+    // create two buttons
+    private Button btnClear = new Button("Clear");
+    private Button btnCalculate = new Button("Calculate");
 
-    // Create a combo box
-    ComboBox combo_box = new ComboBox(FXCollections.observableArrayList(week_days));
+    // Weekdays
+    //String week_days[] =
+   // { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
 
     // create a scene
     Scene sc = new Scene(r, 200, 200);
@@ -62,7 +77,6 @@ public class MeyerGardenerFutureValueApp extends Application {
   }
   
   // Main method only needed for IDE with limited JavaFX support. Not needed for running from command line.
-
   public static void main(String[] args) { 
     launch(args);
   }
