c84a9b2771145c538a8985931f47d09f65350cc4
dmeyergardner <128332832+dmeyergardner@users.noreply.github.com>
2023-05-22 00:56:46 -0400

    Update MeyerGardnerFutureValueApp.java

--- FutureValueApp/FutureValueApp/MeyerGardnerFutureValueApp.java
+++ FutureValueApp/FutureValueApp/MeyerGardnerFutureValueApp.java
@@ -4,7 +4,7 @@
     Modified by R. Krasso 2021
     Additional modifications by D. Meyer-Gardner 2023
  */
-package FutureValueApp;
+package application;
 
 import javafx.application.Application;
 import javafx.geometry.HPos;
@@ -18,15 +18,40 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class MeyerGardnerFutureValueApp extends Application {
+	
+	/*
+     * Create private variables for two TextField’s, one TextArea, five Labels, one ComboBox of type Integer, and two Button’s.
+     */
+    // create two textfield
+    private final TextField txtMonthlyPayment = new TextField();
+    private final TextField txtInterestRate = new TextField();
+
+    //textArea
+    TextArea textArea = new TextArea();
+
+    // create five label
+    private Label lblMonthlyPayment = new Label("Monthly Payment:");
+    private Label lblInterestRate = new Label("Interest Rate:");
+    private Label lblYears = new Label("Years:");
+    private Label lblClear = new Label("Clear");
+    private Label lblCalculate = new Label("Calculate");
+    
+    // create two buttons
+    private Button btnClear = new Button("Clear");
+    private Button btnCalculate = new Button("Calculate");
+    
+    private Label lblInterestRateFormat;
+
 
     //Extend the JavaFX Application object and setup the @Override start method with a single argument for the Stage object 
     @Override // Override the start method in the Application class
     public void start(Stage primaryStage) {
+    	
+    	
         
         /*
             * In the start() method, add a new GridPane object and set its alignment to Pos.CENTER, set its padding to 11.5, 12.5, 13.5, and 14.5. Next, set the panes Hgap to 5.5 and its Vgap to 5.5
@@ -38,63 +63,36 @@ public class MeyerGardnerFutureValueApp extends Application {
         pane.setHgap (5.5);
         pane.setVgap (5.5);
         
-        
-        /*
-         * Create private variables for two TextField’s, one TextArea, five Labels, one ComboBox of type Integer, and two Button’s.
-         */
-        // create two textfield
-        private TextField txtMonthlyPayment = new TextField();
-        private TextField txtInterestRate = new TextField();
-
-        //textArea
-        TextArea textArea = new TextArea();
-
-        // create five label
-        private Label lblMonthlyPayment = new Label("Monthly Payment:");
-        private Label lblInterestRate = new Label("Interest Rate:");
-        private Label lblYears = new Label("Years:");
-        private Label lblClear = new Label("Clear");
-        private Label lblCalculate = new Label("Calculate");
-
         //combobox items of type Integer
         ComboBox<Integer> years = new ComboBox<Integer>();
 
-        // create two buttons
-        private Button btnClear = new Button("Clear");
-        private Button btnCalculate = new Button("Calculate");
-        
         //Add the controls to the GridPane. For the lblInterestRateFormat label, set the text fill to red and HPos to the right.
-        pane.add(lblMonthlyPayment);
-        pane.add(txtMonthlyPayment);
-        lblInterestRateFormat.setTextFill (Color.RED); //Set the font color of the interest rate instructions to red
+        pane.add(lblMonthlyPayment, 1, 0);
+        pane.add(txtMonthlyPayment, 0, 2);
+        lblInterestRateFormat.setTextFill(Color.RED); //Set the font color of the interest rate instructions to red
         pane.add(lblInterestRateFormat, 1, 2); //Add the label to the grid pane
         GridPane.setHalignment(lblInterestRateFormat, HPos.RIGHT); //Postition the label to the right of the pane
 
         //Add the clear and calculate buttons to an HBox with a spacing of 10 and a padding of 15, 0, 15, and 30.
         HBox actionBtnContainer = new HBox(); //Create new Hbox container
-        actionBtnContainer.setPadding (new Insets(15, 0, 15, 30)); //set container padding
-        actionBtnContainer.setSpacing (10); //set container spacing
-        actionBtnContainer.getChildren ()
-
-        .add(btnClear); //add btnClear to container
-        actionBtnContainer.getChildren ()
-
-        .add(btnCalculate); //add btnCalculate to container
-        pane.add (actionBtnContainer, 1, 4); //add the container to the grid panel
-        
-        
+        actionBtnContainer.setPadding(new Insets(15, 0, 15, 30)); //set container padding
+        actionBtnContainer.setSpacing(10); //set container spacing
+        actionBtnContainer.getChildren().add(btnClear); //add btnClear to container
+        actionBtnContainer.getChildren().add(btnCalculate); //add btnCalculate to container
+        pane.add(actionBtnContainer, 1, 4); //add the container to the grid panel
+      
         //Stage and Scene set up
-        Scene scene = new Scene(pane, 700, 300);
+        Scene scene = new Scene(new GridPane(), 700, 300);
         
-        primaryStage.setTitle ("MeyerGardner Future Value App"); // set title for the stage
-        primaryStage.setScene (scene); // Place the scene in the stage
-        primaryStage.show (); // Display the stage
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.setTitle("MeyerGardner Future Value App"); // set title for the stage
+        primaryStage.show(); // Display the stage
+    }
 
     /**
      * @param args the command line arguments
      */
         public static void main(String args) {
         launch(args);
-    }  
     }
 }
