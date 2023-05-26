901cbf76f52a15fc00968e0db242b8ccf1b43a71
dmeyergardner <128332832+dmeyergardner@users.noreply.github.com>
2023-05-25 21:18:27 -0400

    Update MeyerGardnerGradeBookApp.java

--- GradeBookApp/MeyerGardnerGradeBookApp.java
+++ GradeBookApp/MeyerGardnerGradeBookApp.java
@@ -9,16 +9,20 @@ Capstone Project Requirements
 	Name the project folder GradeBookApp.
 	Name the program file YourLastNameGradeBookApp.java.
 	Use GitHub to store the project.
+	
 	Provide users with a grade book form. Include the following fields: first name, last name, course, and grade (use A-F). 
 	For the grade field use a ComboBox with prefilled values.
 	Use labels to identify the forms fields.
 	Provide users with a button to clear the grade book form.
 	Provide users with a button to view saved grade entries.
 	Provide users with a button to save a grade book entry.
+	
 	On form submission, write the entered values to a csv file named grades.csv. 
 	The grades.csv file should have a header row with values for “firstName, lastName, course, and grade.”
 	When a user selects the “view grades’ button, display the contents of the grades.csv file.
+	
 	Use a Java class to hold the values from the gradebook form (firstName, lastName, course, and grade). Name the class Student and include private variables for the fields with two constructors: a default constructor and a parameterized constructor with the class’s properties. Add getter/setter methods for the class properties and Override the toString() method.
+	
 	Use the Student classes toString() method to output the object to the results section of the GUI you create.
 
 	Build the Capstone Project’s user interface using JavaFX.
@@ -32,15 +36,22 @@ Capstone Project Requirements
 
 package application;
 	
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 
 public class MeyerGardnerGradeBookApp extends Application {
@@ -50,20 +61,18 @@ public class MeyerGardnerGradeBookApp extends Application {
 	private Label lblLastName = new Label("Student's Last Name:");
 	private Label lblCourse = new Label("Student's Course:");
 	private Label lblGrade = new Label("Student's Grade:");
-	private Label lblClear = new Label("Clear");
-	private Label lblSave = new Label("Save");
-	private Label lblView = new Label("View Grades");
+	//private Label lblClear = new Label("Clear");
+	//private Label lblSave = new Label("Save");
+	//private Label lblView = new Label("View Grades");
 		
 	// text fields for student first and last name, student course
 	private final TextField txtFirstName = new TextField();
 	private final TextField txtLastName = new TextField();
 	private final TextField txtCourseName = new TextField();
 	
-	// combobox (drop-down) choice options
-	private String[] gradesCbo = {"A", "B", "C", "D", "F"};
-	
 	// combobox (drop-down) items of type string
 	private ComboBox<String> cboGrade = new ComboBox<>();
+	//private String[] gradesCbo = {"A", "B", "C", "D", "F"};
 	
 	// create buttons
 	private Button btnClear = new Button("Clear");
@@ -92,6 +101,14 @@ public class MeyerGardnerGradeBookApp extends Application {
 		pane.add(lblGrade, 0, 4);
 		pane.add(cboGrade, 1, 4);
 		
+		// Add action buttons to an HBox
+        HBox actionBtnContainer = new HBox(10);
+        actionBtnContainer.setPadding(new Insets(15, 0, 15, 30));
+        actionBtnContainer.getChildren().add(btnSave);
+        actionBtnContainer.getChildren().add(btnClear);
+        actionBtnContainer.getChildren().add(btnView);
+        pane.add(actionBtnContainer,1,5);
+		
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
@@ -109,17 +126,26 @@ public class MeyerGardnerGradeBookApp extends Application {
 		txtCourseName.setText("");
         
 		lblGrade.setText("Grades as of " + getCurrentDate());
-        cboGrade.setValue(0);
+        cboGrade.setValue("");
     }
+	
+	private String getCurrentDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        return dateFormat.format(new Date());
+	}
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
 	
-	 @Override
-     public void actionPerformed(ActionEvent e) {
+	 public void actionPerformed(ActionEvent e) {
          // TODO Auto-generated method actionPerformed'");
          calculateResults();
          clearFormFields();
      }
+
+	private void calculateResults() {
+		// TODO Auto-generated method stub
+		
+	}
 }
